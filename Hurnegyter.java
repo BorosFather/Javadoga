675df60cc467518c9d923a8fb83d0a5b6c4053e2
BorosFather <92117990+BorosFather@users.noreply.github.com>
2022-02-22 09:46:15 +0100

    Add files via upload

---
+++ Hurnegyter.java
@@ -0,0 +1,22 @@
+class Hurnegyter{
+ public static void main(String[] args){
+  System.out.println("Boros Zoltán, Szoft I N, 2022,02,22");
+
+  System.out.println("Aoldal:");
+  System.out.println("Boldal:");
+  System.out.println("Coldal:");
+  System.out.println("Doldal:");
+  
+   double felkerulet  = ((Aoldal + Boldal + Coldal + Doldal) / 2);
+   System.out.println( felkerulet);
+
+   double terulet = math.sqrt();
+
+   
+   Sytem.out.print();
+    
+   
+    
+  /(Aoldal + Boldal + Coldal + Doldal) / 2;
+ }
+}
